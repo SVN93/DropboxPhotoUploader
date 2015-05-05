8c89722ea5be586b840375fb6d70574d65b99811
Vladislav <solovev993@gmail.com>
2015-05-05 22:08:36 +0400

    no message

---
+++ DropboxPhotoUploader/SVNImageCell.m
@@ -0,0 +1,13 @@
+//
+//  SVNImageCell.m
+//  DropboxPhotoUploader
+//
+//  Created by Vladislav on 05.05.15.
+//  Copyright (c) 2015 Vladislav. All rights reserved.
+//
+
+#import "SVNImageCell.h"
+
+@implementation SVNImageCell
+
+@end
